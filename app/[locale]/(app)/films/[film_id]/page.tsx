87b93bcefbcc563ce7762fd76d074236f2ef5dd7
zemosamurai <zemosamurai@gmail.com>
2024-06-17 16:42:24 +0300

    feat: add Open Graph

--- app/[locale]/(app)/films/[film_id]/page.tsx
+++ app/[locale]/(app)/films/[film_id]/page.tsx
@@ -1,6 +1,6 @@
 import { FilmPage } from "@/2-pages/film";
 import { filmsApi } from "@/5-entities/films";
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 
 interface IProps {
   params: { film_id: string; locale: string };
@@ -18,13 +18,31 @@ export default function Film({ params: { film_id, locale } }: IProps) {
 //   return res;
 // }
 
-export async function generateMetadata({
-  params: { film_id },
-}: IProps): Promise<Metadata> {
+export async function generateMetadata(
+  { params: { film_id, locale } }: IProps,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
   const film = await filmsApi.getFilm(film_id);
+  const previousImages = (await parent).openGraph?.images || [];
 
   return {
     title: film.result.properties.title,
     description: film.result.description,
+    openGraph: {
+      title: film.result.properties.title,
+      description: film.result.description,
+      url: "http://localhost:8080",
+      siteName: "Next Template",
+      images: [
+        {
+          url: "https://s1.afisha.ru/mediastorage/a0/d9/8733aca357d24d789677ac42d9a0.jpg",
+          width: 800,
+          height: 600,
+        },
+        ...previousImages,
+      ],
+      locale,
+      type: "website",
+    },
   };
 }
